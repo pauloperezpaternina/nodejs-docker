50ed544f1d0d9aa10189ddc99844fd4a58fce3ca
Paulo Perez <pauloperez@bittronic.net>
2022-03-24 12:04:09 -0500

    server ready

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const path = require('path');
 const {json, urlencoded} = express;
 const app = express();
 const port =process.env.PORT || 8080;
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.use('/', (req, res) => {
+
+    res.send('This is the microservice v1');
+
+})
+app.listen(port, () => {
+    console.log('Server listening on port ${port}');
+})
