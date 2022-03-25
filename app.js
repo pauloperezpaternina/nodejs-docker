7a1da73f8da915cec2964e4cc232b67ebef5c838
Paulo Perez <pauloperez@bittronic.net>
2022-03-25 10:41:24 -0500

    index and router var created

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express');
 
 const cors = require('cors');
 const path = require('path');
+const router = require('./src/routes');
 const {json, urlencoded} = express;
 const app = express();
 const port =process.env.PORT || 8080;
+
 const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
+app.use(router);
 app.use('/', (req, res) => {
 
-    res.send('This is the microservice v1');
+    res.send('<h1>This is the microservice v1.0.0</h1>');
 
 })
 app.listen(port, () => {
-    console.log('Server listening on port ${port}');
+    console.log(`Server listening on port ${port}`);
 })
